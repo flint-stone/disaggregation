9f35384b6cc2f3e1f0ce8b0a1982cfdf9b64a535
root <root@node0.lexu-qv71200.dcsq-pg0.clemson.cloudlab.us>
2020-05-08 02:25:53 -0400

    add cloudlab deployment [fix mount point]

---
+++ memeater/memeater.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <unistd.h>
+
+
+int main(int argc, char** argv) {
+    int max = -1;
+    int mb = 0;
+    char* buffer;
+
+    if(argc > 1)
+        max = atoi(argv[1]);
+
+    while((buffer=malloc(1024*1024*1024)) != NULL && mb != max) {
+        memset(buffer, 1, 1024*1024*1024);
+        mb+=1024;
+        printf("Allocated %d MB\n", mb);
+        sleep(1);
+    }      
+    return 0;
+}
--- rmem/.remote_commands/cmd_0.sh
+++ rmem/.remote_commands/cmd_0.sh
@@ -1,5 +1,14 @@
 
-      if [ -n "$(cat /proc/swaps | grep /mnt/swap)" ];
-        then swapoff /mnt/swap; rm /mnt/swap;
-      fi;
-  
+      cd /mydata/lexu/disaggregation/rmem;
+      while [ -n "$(cat /proc/swaps | grep /dev/rmem0)" ];
+        do swapoff /dev/rmem0;
+      done;
+
+      while [ -n "$(lsmod | grep rmem)" ];
+        do rmmod rmem;
+      done;
+
+      while [ -d "swap" ];
+        do rmdir swap;
+      done;
+    
